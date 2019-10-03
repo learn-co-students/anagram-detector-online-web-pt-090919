ed2e0e13a13887cd7edea2a9a5f223fc16f6fede
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 20:39:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(word_array) 
+    word_array.find_all do |w|
+      (@word.split("").sort) == (w.split("").sort)
+      #iterating over the array of words that the .match method takes as an argument which is (word) @word in this case.
+      # split a word into an array of letters using example => some_word.split("") & comparing two arrays using the == .
+    end
+  end
+  
+end 
