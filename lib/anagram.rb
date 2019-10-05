34964b91a164603a9ce617fef5d658f661492889
Srikalyan Megati <kalmegati@gmail.com>
2019-10-05 18:54:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require 'pry'
+
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(phrase)
+    grams = []
+    phrase.each { |piece| grams << piece if piece.split(//).sort == @word.split(//).sort }
+    grams
+  end
+  
+end
