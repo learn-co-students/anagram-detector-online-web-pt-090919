826580b96704397bd0d6339cd9314474a047d9b4
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-07 21:05:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  @@detector = []
+
+  attr_accessor = :anagram
+
+  def initiliaze(anagram)
+    @anagram = anagram
+    @@detector << self
+  end
+
+  def self detect
+    @detector
+  end
+end
+end
