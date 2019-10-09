6e7a4631a6db6d10e04a662794c7782a351d9aea
Anne Fausto <a.r.fausto@gmail.com>
2019-10-09 14:15:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(arr_of_anagrams)
+    arr_of_anagrams.select { |arr| arr.split("").sort == @word.split("").sort }
+  end 
+  
+  
+end 
