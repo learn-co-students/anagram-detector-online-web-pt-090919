b94ac9793346bc67902b8c9d5dbe58a517ec29c4
Ally Morson <ally.morson@gmail.com>
2019-10-20 23:48:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,13 @@
-# Your code goes here!
-class anagram 
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
